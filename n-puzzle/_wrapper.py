20ec7e257dc020bb7e40fe727a50ca9dbc832a7a
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-05-04 22:06:39 -0500

    added _wrapper.py

---
+++ n-puzzle/_wrapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+SET UP EACH EXPERIMENT:
+1. READING OF INPUT FILE (CONTAINING an initial state)
+2. SPECIFY ARGS TO CALL
+3. ITERATIVELY CALL main
+4. RECORD RESULTS TO JSON FILE
+5. name file meaninfully (w datetime?)
+'''
