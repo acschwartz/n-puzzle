b9ac76f0282439d0644678012d2388405803ff11
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-05-06 20:23:46 -0500

    added max(fringe pdb, linear conflicts)

--- n-puzzle/npuzzle/pdb.py
+++ n-puzzle/npuzzle/pdb.py
@@ -6,6 +6,9 @@ PDB_TYPE = None
 # PDB implemented as dictionary
 
 def load_pdb(pdb):
+	def printLoading():
+		print ('Loading PDB ... ')
+	
 	global PATTERN_DATABASE, PDB_TYPE
 	if PATTERN_DATABASE:
 		print("load_pdb: PDB already loaded. Will not re-load.")
@@ -18,32 +21,52 @@ def load_pdb(pdb):
 	if '.json' in filename:
 		from json import load
 		with open(filename, 'r') as f:
+			printLoading()
 			PATTERN_DATABASE = load(f)
 	
 	if '.pickle' in filename:
 		from pickle import load
 		with open(filename, "rb") as f:
+			printLoading()
 			PATTERN_DATABASE = load(f)
 			
 	t_delta = perf_counter() - t_start
 	return t_delta
 
-def pdb_lookup(state, goal_state=None, size=None):
-	
+def pdb_lookup(state, goal_state, size):
 	# this could be implemented cleaner... doing these checks every single call ain't cute
 	# TODO: maybe each pdb should have its own lookup function...
+	
+	if PDB_TYPE == 'max15fringeLC':
+		from npuzzle.heuristics import linear_conflicts
+		lc = linear_conflicts(state, goal_state, size)
+		fringe = pdb_lookup_15fringe(state)
+		return max(fringe, lc)
+		
+	
 	if PDB_TYPE == '15fringe':
+		return pdb_lookup_15fringe(state)
+	
+	if PDB_TYPE == '8puz':
+		try:
+			return PATTERN_DATABASE[state]
+		except NameError:
+			print('pdb.pdb_lookup: attempted lookup but pattern database not loaded')
+			exit(1)
+
+
+def pdb_lookup_15fringe(state):
 #		ptiles = PDBINFO['15fringe']['pattern tiles']
 #		empty_tile = PDBINFO['15fringe']['empty tiles']
 		# TODO: the above should also be passed in as local vars for optimal performance...
 		# global lookups are expensive
-		
+	
 		# just gonna hardcode this in to save lookup time
 		ptiles = (3, 7, 11, 12, 13, 14, 15)
 		empty_tile = 0
 		# I had the line below as pattern = [empty_tile] * 16 and I don't even think that works
 		# since empty_tile is a var... idk looks into it
-		
+	
 		# convert state to pattern
 		pattern = [0] * 16
 		for ptile in ptiles:
@@ -54,18 +77,10 @@ def pdb_lookup(state, goal_state=None, size=None):
 		h = PATTERN_DATABASE[pattern]
 		print('pdb_lookup: h =', h)
 		return h
-	
+
 		# TODO: !!!! so I realized that I should have had the entries be lists in my pdb,
 		# since the lookup keys are patterns and not keys, and patterns have to be built, therefore
 		# are mutable. list <--> tuple conversion takes computation time
-		
-	
-	if PDB_TYPE == '8puz':
-		try:
-			return PATTERN_DATABASE[state]
-		except NameError:
-			print('pdb.pdb_lookup: attempted lookup but pattern database not loaded')
-			exit(1)
 
 
 PDBINFO = {
@@ -75,6 +90,13 @@ PDBINFO = {
 			'goal_state': 'zero_first',
 		},
 	'15fringe': {
+			'file': 	'npuzzle/pdb/15puzzle/4732363__15fringe_database.pickle',
+			'size':		4,
+			'goal_state': 'zero_first',
+			'pattern tiles': (3, 7, 11, 12, 13, 14, 15),
+			'empty tile': 0,
+		},
+	'max15fringeLC':{
 			'file': 	'npuzzle/pdb/15puzzle/4732363__15fringe_database.pickle',
 			'size':		4,
 			'goal_state': 'zero_first',
