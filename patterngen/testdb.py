64b300d4f987c187596c1d6b69f86178e089d7a0
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-05-09 18:33:10 -0500

    added debug statements

--- patterngen/testdb.py
+++ patterngen/testdb.py
@@ -36,12 +36,12 @@ def parseArgs():
 
 if __name__ == '__main__':
 	datasetName, n_entries, DEBUG = parseArgs()
-	
-	DICT = generate_dataset[datasetName](n_entries)
-	
 	con = sqlite3.connect(':memory:')
 	cur = con.cursor()
-
+	
+	DICT = generate_dataset[datasetName](n_entries)
+	maxrss_start = getMaxRSS()
+	time_start = perf_counter()
 	
 	if DEBUG: print('Creating table...')
 	
@@ -73,8 +73,6 @@ if __name__ == '__main__':
 	if DEBUG: print('Inserting values...')
 	
 	
-	maxrss_start = getMaxRSS()
-	time_start = perf_counter()
 	for key in DICT:
 	#	cur.execute(f'INSERT INTO patterncosts VALUES ({key}, {DICT[key]})')	# <-- didn't work for strings
 		cur.execute("""INSERT INTO patterncosts(pstring, cost) 
@@ -84,7 +82,8 @@ if __name__ == '__main__':
 	
 	time_delta = timeDelta(time_start)
 	maxrss_after_populate_table = getMaxRSS()
-	maxrss_delta_pretty = rawMaxRSStoPrettyString(maxrss_after_populate_table - maxrss_start)
+	maxrss_delta = maxrss_after_populate_table - maxrss_start
+	maxrss_delta_pretty = rawMaxRSStoPrettyString(maxrss_delta)
 	
 	res = cur.execute("SELECT * from patterncosts LIMIT 1")
 	for row in res:
@@ -95,12 +94,14 @@ if __name__ == '__main__':
 		for row in res:
 			print(row)
 	
-	if DEBUG: print(f'maxrss_after_populate_table: {maxrss_after_populate_table}')
+	if DEBUG: print(f'maxrss_start: {maxrss_start}\t{rawMaxRSStoPrettyString(maxrss_start)}')
+	if DEBUG: print(f'maxrss_after_populate_table: {maxrss_after_populate_table}\t{rawMaxRSStoPrettyString(maxrss_after_populate_table)}')
+	if DEBUG: print(f'maxrss_delta: {maxrss_delta}\t{rawMaxRSStoPrettyString(maxrss_delta)}')
 	
 #	print('\n')
 #	print(f'\n{SEPARATOR_SM}')
 	print(f'\nPrimary key type: {datasetName}\t\te.g. {example_row}')
 #	print(SEPARATOR_SM)
 	print(f'{prettyTime(time_delta)} to insert {n_entries} entries')
-	print(f'memory used: {maxrss_delta_pretty}')
+	print(f'memory used (to store DB): {maxrss_delta_pretty}')
 #	print(SEPARATOR)
