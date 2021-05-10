bfe714b60520d45b5633935d2df68944a09258f2
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-05-09 20:52:31 -0500

    updated ints_repr_hexvalues to be more representative of avg case

--- patterngen/dummyvalues.py
+++ patterngen/dummyvalues.py
@@ -16,10 +16,16 @@
 ###==============================================================================================##
 ## e.g.  935124720  (representing '0x37bcdef0')
 def ints_repr_hexvalues(n_entries, max_cost=100):
-	#first_key = 935124720 # (representing '0x37bcdef0')
-	first_key = 4260149040 # (representing '0xfdecb730' which is the numerically biggest repr for this pattern)
+	from math import floor
+	first_key = 935124720 # (representing '0x37bcdef0')
 	ints_repr_hexvalues = {}
-	for n in range(n_entries):
+	for n in range(floor(n_entries/2)):
+		cost = n % max_cost
+		next_key = first_key + n
+		ints_repr_hexvalues[next_key] = cost
+	
+	first_key = 4260149040 # (representing '0xfdecb730' which is the numerically biggest repr for this pattern)
+	for n in range(floor(n_entries/2), n_entries):
 		cost = n % max_cost
 		next_key = first_key - n
 		ints_repr_hexvalues[next_key] = cost
