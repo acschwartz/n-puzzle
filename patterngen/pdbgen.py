ae32c60a58a549072de52b59600496a4271218f1
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-05-10 05:31:28 -0500

    addd parser to new pdbgen.py

--- patterngen/pdbgen.py
+++ patterngen/pdbgen.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 ##==============================================================================================##
-import logging
+import logging	#probably put into separate module
+from time import strftime
 
 from helpers.memorytools import *
 from helpers.timetools import *
 
 from pdbgen import moves
+from pdbgen.parser import parseArgs
 from pdbgen import patterns
 
 #mmaybe remove later
@@ -14,3 +16,13 @@ from pprint import pp as pp
 
 RUN_ID = strftime(f'%y%m%d-%H%M%S')
 OUTPUT_DIRECTORY = 'output/'
+
+
+##==============================================================================================##
+#		M A I N
+##==============================================================================================##
+if __name__ == '__main__':
+	pname = parseArgs()
+	print(f'pname: {pname}')
+	print(f'RUN_ID: {RUN_ID}')
+	
