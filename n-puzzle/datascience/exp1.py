f11603a1ca5ff4cb72aa416bece31f65d2afa92f
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-08-13 18:16:39 -0500

    exp1: set up pandas dataframe w/ all fields. todo: set data types

--- n-puzzle/datascience/exp1.py
+++ n-puzzle/datascience/exp1.py
@@ -1,53 +1,77 @@
 from collections import namedtuple
 import pandas as pd
 
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
 from get_heuristic_value import get_h_value
 
 
-experiment = 1
+to_process = [
+    {
+        'experiment': 1,
+        'timeout (min)': None,
 
-inputFilename = 'data/Experiment 1 (100 8-puzzles)/8puz_AStar_h1___May12-2021_02-37-50PM.json'
-df = pd.read_json(inputFilename)
-df = df.transpose()
-
-
-del df['foundSol']
-
-df = df.rename(columns={
-    'algo': 'algo',
-    'init': 'puzzle',
-    'goal': 'goal',
-    'max_path_len': 'search depth',
-    'nodes_gen': 'nodes generated',
-    'runtime_sec': 'runtime (sec)',
-    'sol_len': 'sol length',
-    'timedOut': 'timed out?'
-    })
-
-df.insert(0, "exp", [experiment]*len(df), False)
-df.insert(1, "N", [len(eval(df.loc[1, 'puzzle']))-1]*len(df), False)    # N = 8, 15, etc..
-
-
-# puzzle  algo  heuristic  'h val'  'sol length'  'timed out?'  'timeout (min)'  'runtime (sec)'  'nodes generated'  'search depth'  'time complexity (nodes)'  'space complexity (nodes)' 
-
-'''
-if algo == 'A*':
-    time complexity = nodes generated
-    space complexity = nodes generated
-else:  # IDA* family
-    time complexity = nodes generated
-    space complexity = search depth
-'''
-
-
-
-HeuristicNames = namedtuple('HeuristicNames', ['fn', 'solverpy', 'dataframe', 'desc'])
-h = [
-    None,
-    HeuristicNames('h1', solverpy='hamming', dataframe='hamming', desc='misplaced tiles'),
-    HeuristicNames('h2', 'manhattan', 'manhattan', desc=None),
-    HeuristicNames('h3', 'linearconflicts', 'MHD + linear conflicts', desc='MHD + linear conflicts')
+        'file': 'data/Experiment 1 (100 8-puzzles)/8puz_AStar_h1___May12-2021_02-37-50PM.json',
+        'h': 'h1',
+    },
 ]
 
-# class DFBuilder:
-#     def __init__(self, filename, heuristic, timeout_min, experiment=None, n_puzzle=None):
+
+HeuristicInfo = namedtuple('HeuristicNames', ['solverpy', 'fordataframe', 'desc'])
+heuristics = {
+    'h1': HeuristicInfo(solverpy='hamming', fordataframe='hamming', desc='misplaced tiles'),
+    'h2': HeuristicInfo('manhattan', 'manhattan', desc=None),
+    'h3': HeuristicInfo('linearconflicts', 'MHD + linear conflicts', desc='MHD + linear conflicts')
+}
+
+
+for metadata in to_process:
+    df = pd.read_json(metadata['file'])
+    df = df.transpose()
+
+    del df['foundSol']
+    del df['timedOut']
+
+    df = df.rename(columns={
+        'algo': 'algo',
+        'init': 'puzzle',
+        'goal': 'goal',
+        'max_path_len': 'search depth',
+        'nodes_gen': 'nodes gen',
+        'runtime_sec': 'runtime (sec)',
+        'sol_len': 'sol length',
+        })
+
+
+    # populate new fields
+    df.insert(0, "exp", [metadata['experiment']] * len(df), False)
+    df.insert(1, "N", [len(eval(df.loc[1, 'puzzle']))-1] * len(df), False)    # N = 8, 15, etc..
+    df.insert(2, 'heuristic', [heuristics[metadata['h']].fordataframe] * len(df), False)
+    df.insert(3, 'timeout (min)', [metadata['timeout (min)']] * len(df), False)
+    df['time (nodes)'] = df['nodes gen']
+    df['space (nodes)'] = df['nodes gen'].where(df['algo'] == 'A*', df['search depth'])
+    df['h val'] = list(map(lambda puz: get_h_value(heuristics[metadata['h']].solverpy, puz), df['puzzle'].to_list() ))
+
+    column_order = [
+        'exp',
+        'N',
+        'algo',
+        'heuristic',
+        'h val',
+        'sol length',
+        'timeout (min)',   # TODO: not sure where this goes
+        'runtime (sec)',
+        'time (nodes)',
+        'space (nodes)',
+        'puzzle',
+        'goal',             # TODO: not sure where this goes
+        'nodes gen', 
+        'search depth',
+    ]
+
+    df = df[column_order]
