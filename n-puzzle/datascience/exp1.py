cf8d36830ada5424b502ddeaeeb604e727b74317
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-08-13 16:34:45 -0500

    normalizing data: changed all sol length = 0 to =-1

--- n-puzzle/datascience/exp1.py
+++ n-puzzle/datascience/exp1.py
@@ -1,16 +1,34 @@
+from collections import namedtuple
 import pandas as pd
 
+from get_heuristic_value import get_h_value
+
+
+experiment = 1
+
 inputFilename = 'data/Experiment 1 (100 8-puzzles)/8puz_AStar_h1___May12-2021_02-37-50PM.json'
-
-
 df = pd.read_json(inputFilename)
 df = df.transpose()
-# transposing because pd dataframe format in JSON is:
-# {'col1': {'row1': ..., 'row2': ...}, 'col2': {'row1': ..., 'row2': ... }}
 
 
+del df['foundSol']
 
-# puzzle  algo  heuristic  'h val'  'solution length'  'timed out?'  'timeout (min)'  'runtime (sec)'  'nodes generated'  'search depth'  'time complexity (nodes)'  'space complexity (nodes)' 
+df = df.rename(columns={
+    'algo': 'algo',
+    'init': 'puzzle',
+    'goal': 'goal',
+    'max_path_len': 'search depth',
+    'nodes_gen': 'nodes generated',
+    'runtime_sec': 'runtime (sec)',
+    'sol_len': 'sol length',
+    'timedOut': 'timed out?'
+    })
+
+df.insert(0, "exp", [experiment]*len(df), False)
+df.insert(1, "N", [len(eval(df.loc[1, 'puzzle']))-1]*len(df), False)    # N = 8, 15, etc..
+
+
+# puzzle  algo  heuristic  'h val'  'sol length'  'timed out?'  'timeout (min)'  'runtime (sec)'  'nodes generated'  'search depth'  'time complexity (nodes)'  'space complexity (nodes)' 
 
 '''
 if algo == 'A*':
@@ -21,35 +39,15 @@ else:  # IDA* family
     space complexity = search depth
 '''
 
-del df['foundSol']
-df = df.rename(columns={
-    'algo': 'Algo',
-    'init': 'Puzzle',
-    'goal': 'Goal',
-    'max_path_len': 'Search depth',
-    'nodes_gen': 'Nodes generated',
-    'runtime_sec': 'Runtime (sec)',
-    'sol_len': 'Solution length',
-    'timedOut': 'Timed out?'
-    })
 
-df = df.rename(columns={
-    'algo': 'algo',
-    'init': 'puzzle',
-    'goal': 'goal',
-    'max_path_len': 'search depth',
-    'nodes_gen': 'nodes generated',
-    'runtime_sec': 'runtime (sec)',
-    'sol_len': 'solution length',
-    'timedOut': 'timed out?'
-    })
 
-heuristicNames = {
-    'perfect': 'perfect',
-    'h1': 'hamming',  # misplaced tiles
-    'h2': 'manhattan',
-    'h3': 'MHD + linear conflicts',  # manhattan + linear conflicts
-}
+HeuristicNames = namedtuple('HeuristicNames', ['fn', 'solverpy', 'dataframe', 'desc'])
+h = [
+    None,
+    HeuristicNames('h1', solverpy='hamming', dataframe='hamming', desc='misplaced tiles'),
+    HeuristicNames('h2', 'manhattan', 'manhattan', desc=None),
+    HeuristicNames('h3', 'linearconflicts', 'MHD + linear conflicts', desc='MHD + linear conflicts')
+]
 
-class DFBuilder:
-    def __init__(self, filename, heuristic, timeout_min, experiment=None, n_puzzle=None):
+# class DFBuilder:
+#     def __init__(self, filename, heuristic, timeout_min, experiment=None, n_puzzle=None):
