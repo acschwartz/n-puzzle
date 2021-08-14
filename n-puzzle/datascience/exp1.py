f129ccf9828a367bf30a247326ccff7562224a96
acschwartz <31809318+acschwartz@users.noreply.github.com>
2021-08-14 00:05:11 -0500

    datascience/exp1: added data types, heuristic % error

--- n-puzzle/datascience/exp1.py
+++ n-puzzle/datascience/exp1.py
@@ -35,14 +35,18 @@ def calcEffectiveBranchingFactor(df, row_index):
         mask = np.logical_and(np.isreal(roots), roots > 1)
         bstar = float(roots[mask][0])   # hopefully there is only one!
 
-        return bstar
+        return np.around(bstar, 2)
+
+def calcHeuristicPercentError(df, row_index):
+    i = row_index
+    percent_error = np.abs( df['h val'][i] - df['sol length'][i] ) / df['sol length'][i] * 100
+    return np.around(percent_error, 1)
 
 
 to_process = [
     {
         'experiment': 1,
-        'timeout (min)': None,
-
+        'timeout (min)': pd.NA,
         'file': 'data/Experiment 1 (100 8-puzzles)/8puz_AStar_h1___May12-2021_02-37-50PM.json',
         'h': 'h1',
     },
@@ -74,7 +78,6 @@ for metadata in to_process:
         'sol_len': 'sol length',
         })
 
-
     # populate new fields
     df.insert(0, "exp", [metadata['experiment']] * len(df), False)
     df.insert(1, "N", [len(eval(df.loc[1, 'puzzle']))-1] * len(df), False)    # N = 8, 15, etc..
@@ -84,43 +87,50 @@ for metadata in to_process:
     df['space (nodes)'] = df['nodes gen'].where(df['algo'] == 'A*', df['search depth'])
     df['h val'] = list(map(lambda puz: get_h_value(heuristics[metadata['h']].solverpy, puz), df['puzzle'].to_list() ))
 
+
+    # convert sol length = -1 to null (for ability to easily query % non-null)
+    df['sol length'] = df['sol length'].where(df['sol length'] > 0, pd.NA)
+
+    # round runtimes
+    df['runtime (sec)'] = [np.around(df['runtime (sec)'][i], 1) for i in range(1, len(df)+1)]
     
-    # calculate effective branching factor
-    list_nodesgen = df['nodes gen'].to_list()
-    list_depth = df['search depth'].to_list()
+    # calculate effective branching factor (b*)
+    df['b*'] = [ calcEffectiveBranchingFactor(df, i) for i in range(1, len(df)+1) ]
 
-    list_bstar = [] # effective branching factor
-    for i in range(1, len(df)+1):
-        list_bstar.append(calcEffectiveBranchingFactor(df, i))
-    
-    df['b*'] = list_bstar
+    # calculate heuristic % error
+    df['h % error'] = [ calcHeuristicPercentError(df, i) for i in range(1, len(df)+1) ]
 
+    cols = {
+        'exp': 'Int64',
+        'algo': 'string',
+        'heuristic': 'string',
+        'h val': 'Int64',
+        'sol length': 'Int64',
+        'h % error': 'Float64',
+        'timeout (min)': 'Int64',
+        'runtime (sec)': 'Float64',
+        'time (nodes)': 'Int64',
+        'space (nodes)': 'Int64',
+        'b*': 'Float64',
+        'nodes gen': 'Int64', 
+        'search depth': 'Int64',
+        'puzzle': 'string',
+        'N': 'Int64',
+        'goal': 'string',
+    }
 
-    # re-order cols
-    column_order = [
-        'exp',
-        'N',
-        'algo',
-        'heuristic',
-        'h val',
-        'sol length',
-        'timeout (min)',   # TODO: not sure where this goes
-        'runtime (sec)',
-        'time (nodes)',
-        'space (nodes)',
-        'b*',
-        'nodes gen', 
-        'search depth',
-        'puzzle',
-        'goal',             # TODO: not sure where this goes
-    ]
-    df = df[column_order]
+    # re-order columns
+    df = df[list(cols.keys())]
+
+    # set column data types
+    df = df.astype(cols)
 
     # display max columns 
     pd.set_option('display.max_columns', None)
     print(df)
-    
-    
+    print(df.info())
+  
 
+df.astype(cols).dtypes
 
     
